c47efc0157873e64cd5a90d593374563658e2c25
TOPKAT <webmaster@nicewebagence.com>
2025-04-24 16:45:16 +0200

    first commit

---
+++ project/astro.config.mjs
@@ -0,0 +1,20 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import mdx from '@astrojs/mdx';
+import alpinejs from '@astrojs/alpinejs';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    tailwind(), 
+    mdx(), 
+    alpinejs()
+  ],
+  markdown: {
+    shikiConfig: {
+      theme: 'github-dark',
+      wrap: true
+    }
+  }
+});
