cabe8594e4323cee2e687724b25147e40d1a0546
TOPKAT <webmaster@nicewebagence.com>
2025-06-20 19:02:14 +0200

    Doc new version

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
-import react from '@astrojs/react';
+import { defineConfig } from 'astro/config'
+import starlight from '@astrojs/starlight'
+import react from '@astrojs/react'
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from '@tailwindcss/vite'
 
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://stargazers.club', // TODO replace when in prod
+	site: 'https://greendotjs.com', // TODO replace when in prod
 	integrations: [
 		react(),
 		starlight({
@@ -18,21 +18,36 @@ export default defineConfig({
 				Hero: './src/components/SiteHero.astro',
 			},
 			editLink: {
-				baseUrl: 'https://github.com/withastro/starlight/edit/main/docs/', // TODO
+				baseUrl: 'https://github.com/topkat/green_dot', // TODO
 			},
-			logo: { src: './src/assets/logo.png' },
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			logo: { src: './src/assets/green_dot-logo.png' },
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/topkat/green_dot' }],
 			sidebar: [
 				{
 					label: 'Guides',
 					items: [
 						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
+						{ label: 'Quick Start', slug: 'guides/quickstart' },
+						{ label: 'Key Concepts', slug: 'guides/main-concepts' },
+						{ label: 'API Services', slug: 'guides/api-services' },
+						{ label: 'Using Database', slug: 'guides/using-database' },
+						{ label: 'Models And DAOs', slug: 'guides/models-and-daos' },
+						{ label: 'Context Object (ctx)', slug: 'guides/ctx' },
+						{ label: 'Error Handling', slug: 'guides/error-handling' },
+						{ label: 'Tips and Helpers', slug: 'guides/tips-and-helpers' },
+						{ label: 'Schedules', slug: 'guides/schedules' },
+						{ label: 'Hooks', slug: 'guides/hook-service' },
+						{ label: 'Usage in Frontend', slug: 'guides/frontend-usage' },
 					],
 				},
 				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
+					label: 'Plugins',
+					autogenerate: { directory: 'plugins' },
+					// items: [
+					// 	{ label: 'API Key Authentication', slug: 'plugins/api-key-authentication' },
+					// 	{ label: 'Double Authentication', slug: 'plugins/double-authentication' },
+					// 	{ label: 'Managed Authentication', slug: 'plugins/managed-authentication' },
+					// ],
 				},
 			],
 			customCss: [
@@ -41,7 +56,7 @@ export default defineConfig({
 				'@fontsource/dm-mono/400.css',
 				'@fontsource/dm-mono/500.css',
 				// Path to your Tailwind base styles:
-				'./src/styles/global.css',
+				'./src/global.css',
 			],
 		}),
 	],
@@ -49,4 +64,4 @@ export default defineConfig({
 	vite: {
 		plugins: [tailwindcss()],
 	},
-});
+})
